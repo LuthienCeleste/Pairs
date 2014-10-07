40190119dc9dcf7cff77569e9e835369ada8109a
Jose Antonio Dura Olmos <jadura@gmail.com>
2014-10-07 17:35:25 +0200

    Localize GameOverScene. Not working
    Text gets out of screen. Will have to move it
    to new UI system.

---
+++ Assets/LocalizeGUIText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using SmartLocalization;
+
+public class LocalizeGUIText : MonoBehaviour {
+    public string key;
+
+    	// Use this for initialization
+	void Start () {
+        Lang lang = GameObject.Find ("Lang").GetComponent<Lang> ();
+        guiText.text = lang.get (key);
+	}
+	
+}
--- Assets/Scripts/LevelReached.cs
+++ Assets/Scripts/LevelReached.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class LevelReached : MonoBehaviour {
-	// Use this for initialization
+
+    // Use this for initialization
 	void Start () {
         G g = GameObject.Find ("G").GetComponent<G> ();
         GameObject text = GameObject.Find ("GUI Text Level XX");
-        text.guiText.text = "LEVEL " + (g.getLevel () - 1);	
+        Lang lang = GameObject.Find ("Lang").GetComponent<Lang> ();
+
+        text.guiText.text = lang.get ("LEVEL") + " " + (g.getLevel () - 1);	
 	}
 	
 }
